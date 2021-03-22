e568d1a5865fdb9257b9e092b9fd37fb28c5f05e
Simone Giovo <sgiovo@e3r10p1.42roma.it>
2021-03-22 17:52:02 +0100

    vera_norma

--- parsing2.c
+++ parsing2.c
@@ -12,28 +12,28 @@
 
 #include "pushswap.h"
 
-int     mparse_helper(char **argv, char **split, int  i, int *out)
+int		mparse_helper(char **argv, char **split, int i, int *out)
 {
-    int		k;
-    int     j;
+	int		k;
+	int		j;
 
-    j = 0; 
-    k = 0;
-    while (argv[++k])
-    {
-    	split = ft_split(argv[k], ' ');
-    	i = -1;
-    	while (split[++i])
-    	{
-    		if (ft_strnbr(split[i]))
-    			out[j++] = ft_atoi(split[i]);
-    		else if (!is_a_flag(split[i]))
-    		{
-    			ft_printf("errore di formato!! |%s|\n", split[i]);
-    			exit(0);
-    		}	
-    	}
-    	ft_free_matrix((void **)split, ft_matrix_len(split));
-    }
-    return j;
+	j = 0;
+	k = 0;
+	while (argv[++k])
+	{
+		split = ft_split(argv[k], ' ');
+		i = -1;
+		while (split[++i])
+		{
+			if (ft_strnbr(split[i]))
+				out[j++] = ft_atoi(split[i]);
+			else if (!is_a_flag(split[i]))
+			{
+				ft_printf("errore di formato!! |%s|\n", split[i]);
+				exit(0);
+			}
+		}
+		ft_free_matrix((void **)split, ft_matrix_len(split));
+	}
+	return (j);
 }
